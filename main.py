9566383cda21787d8a5b8a61beaddfaaa107b570
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 13:29:02 -0600

    generate authentication request URL

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-import getpass
-
 api_id = "08bd2d1f3c4d645cdfa32071978a4a1c"
 api_secret = "a953130215e15c58c1e27c5b3ff4f41e"
 
+def URL_init(body):
+    URL = "https://" + body + "?"
+    return URL
+
+def URL_add_parameter(prefix, param, value):
+    previous_char = prefix[len(prefix) - 1]
+    if previous_char == '?':
+        URL = prefix + param + "=" + value
+    else:
+        URL = prefix + "&" + param + "=" + value
+    return URL
+
 def authenticate():
-    print("Please enter your 23andMe credentials.")
-    username = input("Username: ")
-    password = getpass.getpass("Password: ");
-    
-    # testing:
-    print("Your username is {0} and your password is {1}.".format(username, password))
+    # variables
+    redirect_URI = "http://localhost:5000/receive_code/"
+    scope_list = "basic names genome phenotypes:read:all"
+    global api_id
+    # construct authentication URL
+    auth_URL = URL_init("api.23andme.com/authorize/")
+    auth_URL = URL_add_parameter(auth_URL, "response_type", "code")
+    auth_URL = URL_add_parameter(auth_URL, "client_id", api_id)
+    auth_URL = URL_add_parameter(auth_URL, "scope", scope_list)
+    # testing
+    print(auth_URL)
 
 def main():
     authenticate()
