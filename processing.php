6c10902cf99a2c4522da4e9405b55afedcde49dc
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 17:13:53 -0600

    add debugging code

--- processing.php
+++ processing.php
@@ -1,4 +1,4 @@
-<?php
+<?php
 
 $client_id = "08bd2d1f3c4d645cdfa32071978a4a1c";
 $client_secret = "a953130215e15c58c1e27c5b3ff4f41e";
@@ -29,6 +29,8 @@ $postHeader = array(
   'scope: ' . 'basic%20rs3094315'
 );
 
+echo "We made it this far";
+
 
 
         $ch = curl_init('https://api.23andme.com/token/' . ' -d client_id=' . $client_id . ' -d client_secret=' . $client_secret . "  -d grant_type=authorization_code -d code=zzz -d 'redirect_uri=https://localhost:5000/receive_code/' -d 'scope=basic%20rs3094315'");
@@ -38,6 +40,7 @@ $postHeader = array(
             CURLOPT_RETURNTRANSFER => TRUE,
             CURLOPT_HTTPHEADER => json_encode($postHeader)
         ));
+        echo "We made it this far 2";
         $response = curl_exec($ch);
 
         // Check for errors
@@ -45,14 +48,14 @@ $postHeader = array(
             echo "ERROR";
             die(curl_error($ch));
         }
-
+echo "We made it this far 3";
         // Decode the response
         $responseData = json_decode($response, TRUE);
 
         // Print the date from the response
         echo $responseData['published'];
 
-
+echo "Hello world";
 
 ?>
 
