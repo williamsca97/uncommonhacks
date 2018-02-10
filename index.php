8a5d54088ddbca5835b84789ec2b9964d3a441fa
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 14:05:12 -0600

    start integrating backend with frontend

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $command = escapeshellcmd('python/get_authentication_url.py');
+    $output = shell_exec($command)
+
+<html>
+    <head>
+        <title>Musical DNA Project</title>
+    </head>
+    <body>
+        <p>Welcome to the Musical DNA Project! <a href="<?php echo $output ?>">Click here</a> to get started.</p>
+    </body>
+</html>
