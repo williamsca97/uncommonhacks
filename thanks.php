775db9b2a71bfb41da27027efabb48b49131fec4
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 17:10:25 -0600

    blah

--- thanks.php
+++ thanks.php
@@ -11,49 +11,49 @@ echo <<<HTML
 <p>Click here to continue to the next step of authentication: <a href="http://connorw.me/hackathon/uncommonhacks/processing.php?code=$auth_code">CLICK HERE</a></p>
 HTML;
 
-$postData = array(
-    'client_id' => $client_id,
-    'client_secret' => $client_secret,
-    'grant_type' => 'authorization_code',
-    'code' => 'zzz',
-    'redirect_uri' => $redirect_URI,
-    'scope' => 'basic%20rs3094315'
-);
+#$postData = array(
+#    'client_id' => $client_id,
+#    'client_secret' => $client_secret,
+#    'grant_type' => 'authorization_code',
+##    'code' => 'zzz',
+#    'redirect_uri' => $redirect_URI,
+ #   'scope' => 'basic%20rs3094315'
+#);
 
-$postHeader = array(
-  'client_id: ' . $client_id,
-  'client_secret: ' . $client_secret,
-  'grant_type: authorization_code',
-  'code: zzz',
-  'redirect_uri: ' . $redirect_URI,
-  'scope: ' . 'basic%20rs3094315'
-);
+#$postHeader = array(
+#  'client_id: ' . $client_id,
+#  'client_secret: ' . $client_secret,
+#  'grant_type: authorization_code',
+#  'code: zzz',
+#  'redirect_uri: ' . $redirect_URI,
+#  'scope: ' . 'basic%20rs3094315'
+#);
 
-if (!isset($_GET['scope']) {
+#if (!isset($_GET['scope']) {
 
         
-        $ch = curl_init('https://api.23andme.com/token/' . ' -d client_id=' . $client_id . ' -d client_secret=' . $client_secret . "  -d grant_type=authorization_code -d code=zzz -d 'redirect_uri=https://localhost:5000/receive_code/' -d 'scope=basic%20rs3094315'");
+ #       $ch = curl_init('https://api.23andme.com/token/' . ' -d client_id=' . $client_id . ' -d client_secret=' . $client_secret . "  -d grant_type=authorization_code -d code=zzz -d 'redirect_uri=https://localhost:5000/receive_code/' -d 'scope=basic%20rs3094315'");
         #);
-        curl_setopt_array($ch, array(
-            CURLOPT_POST => TRUE,
-            CURLOPT_RETURNTRANSFER => TRUE,
-            CURLOPT_HTTPHEADER => json_encode($postHeader)
-        ));
-        $response = curl_exec($ch);
+  #      curl_setopt_array($ch, array(
+   #         CURLOPT_POST => TRUE,
+    #        CURLOPT_RETURNTRANSFER => TRUE,
+     #       CURLOPT_HTTPHEADER => json_encode($postHeader)
+      #  ));
+       # $response = curl_exec($ch);
         
         // Check for errors
-        if($response === FALSE){
-            echo "ERROR";
-            die(curl_error($ch));
-        }
+        #if($response === FALSE){
+         #   echo "ERROR";
+          #  die(curl_error($ch));
+#        }
         
         // Decode the response
-        $responseData = json_decode($response, TRUE);
+ #       $responseData = json_decode($response, TRUE);
         
         // Print the date from the response
         #echo $responseData['published'];
-}
+#}
 
-echo "hello world";
+#echo "hello world";
 
 ?>
