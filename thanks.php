6fa41f2c8c86e241ba03f95f17969b0a2a76fd5c
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 16:52:49 -0600

    prevent infinite looping of thanks.php

--- thanks.php
+++ thanks.php
@@ -2,7 +2,7 @@
 
 $client_id = "08bd2d1f3c4d645cdfa32071978a4a1c";
 $client_secret = "a953130215e15c58c1e27c5b3ff4f41e";
-$redirect_URI = "http://connorw.me/hackathon/uncommonhacks/thanks2.php";
+$redirect_URI = "http://connorw.me/hackathon/uncommonhacks/thanks.php";
 $auth_code = htmlspecialchars($_GET["code"]);
 
 echo 'You have successfully authenticated with our application. The authentication code is ' . $auth_code . '.';
