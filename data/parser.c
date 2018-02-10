85a581f8a873de5b59d8af4604cd7934a2e62578
aaboaf <aaboaf@uchicago.edu>
2018-02-10 16:24:49 -0600

    Added beginnings of parser

---
+++ data/parser.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+int main(int argc, char *argv[])
+{
+    if(argc <= 1)
+    {
+        return 0; 
+    }
+
+    char *filename = argv[1];
+    FILE *fp = fopen(filename, "r"  );
+    
+    char buffer[1024]; // Buffer for storing line by line information. Probably smarter to make this smaller and overwrite it every pass, instead storing the information directly in whatever format we need immediately
+
+    while(fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
+        puts(buffer);
+        printf("Buffer: %s\n");
+    }
+    
+    fclose(fp);
+    
+
+
+    
+}
