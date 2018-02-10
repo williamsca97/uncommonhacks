19ef671dbee9c5b2ffbb67e7c078b9f2fc276219
Jake Zane <jzane16@kentdenver.org>
2018-02-10 16:06:18 -0600

    progress on php stuff

--- thanks.php
+++ thanks.php
@@ -3,6 +3,42 @@
 $client_id = "08bd2d1f3c4d645cdfa32071978a4a1c";
 $client_secret = "a953130215e15c58c1e27c5b3ff4f41e";
 $redirect_URI = "http://connorw.me/hackathon/uncommonhacks/thanks.php";
+$auth_code = htmlspecialchars($_GET["code"]);
 
-echo 'You have successfully authenticated with our application. The authentication code is ' . htmlspecialchars($_GET["code"]) . '.';
+echo 'You have successfully authenticated with our application. The authentication code is ' . $auth_code . '.';
+
+$postData = array(
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'grant_type' => 'authorization_code',
+    'code' => 'zzz',
+    'redirect_uri' => $redirect_URI,
+    'scope' => 'basic%20rs3094315'
+);
+
+$postHeader = array(
+  'client_id: ' . $client_id,
+  'client_secret: ' . $client_secret,
+)
+
+$ch = curl_init('https://api.23andme.com/token/');
+curl_setopt_array($ch, array(
+    CURLOPT_POST => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_POSTFIELDS => json_encode($postData)
+));
+$response = curl_exec($ch);
+
+// Check for errors
+if($response === FALSE){
+    echo "ERROR";
+    die(curl_error($ch));
+}
+
+// Decode the response
+$responseData = json_decode($response, TRUE);
+
+// Print the date from the response
+echo $responseData['published'];
+
 ?>
