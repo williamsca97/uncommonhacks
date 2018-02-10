1ca11ba70de17c35c2fd714d3b5ea7d2464c0410
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 12:54:20 -0600

    start working on authentication

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import getpass
+
+api_id = "08bd2d1f3c4d645cdfa32071978a4a1c"
+api_secret = "a953130215e15c58c1e27c5b3ff4f41e"
+
+def authenticate():
+    print("Please enter your 23andMe credentials.")
+    username = input("Username: ")
+    password = getpass.getpass("Password: ");
+    
+    # testing:
+    print("Your username is {0} and your password is {1}.".format(username, password))
+
 def main():
-    print("Hello world!")
+    authenticate()
 
 if __name__ == "__main__":
     main()
