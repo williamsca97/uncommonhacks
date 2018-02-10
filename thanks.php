8645f72fbc31159f5c98eed9f31be548eaa37f82
Connor Atkinson Williams <williamsca@uchicago.edu>
2018-02-10 15:04:48 -0600

    added keys to thanks.php

--- thanks.php
+++ thanks.php
@@ -1,3 +1,8 @@
 <?php
+
+$client_id = "08bd2d1f3c4d645cdfa32071978a4a1c";
+$client_secret = "a953130215e15c58c1e27c5b3ff4f41e";
+$redirect_URI = "http://connorw.me/hackathon/uncommonhacks/thanks.php";
+
 echo 'You have successfully authenticated with our application. The authentication code is ' . htmlspecialchars($_GET["code"]) . '.';
 ?>
